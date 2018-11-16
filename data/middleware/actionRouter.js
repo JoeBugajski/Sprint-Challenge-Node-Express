6d683de11a3d821aa8fe7eb545f568a2438a64d0
Lambda_School_Loaner_27 <lambda_school_loaner_27@Lambda-School-Loaner-27s-MacBook-Pro.local>
2018-11-16 13:02:44 -0600

    added a bunch of conditionals to action post, ready to test

--- data/middleware/actionRouter.js
+++ data/middleware/actionRouter.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const db = require('../helpers/actionModel.js');
+const projectDb = require('../helpers/projectModel.js');
 
 router.get('/', (req, res) => {
   db.get()
@@ -30,5 +31,27 @@ router.get('/:id', (req, res) => {
         .json({ error: "Problem getting that action..." })
     })
 });
+
+router.post('/', (req, res) => {
+  const action = req.body;
+  const projId = action.project_id;
+  const associateProject = projectDb.get(projId);
+  if (!associateProject) {
+    res
+      .status(404)
+      .json({ message: "The project ID associated with this action does not exist..." })
+  } 
+  else if ((action.description.length > 128 || action.description.length === 0) || typeOf(action.description) !== 'string') {
+    res 
+      .status(406)
+      .json({ message: "Please make sure the action description is less than 128 characters, \nand greater than zero characters" })
+  }
+  else if (action.notes.length === 0 || typeOf(action.notes) !== 'string') {
+    res
+      .status(406)
+      .json({ message: "Please make sure the length of the 'notes' section is greater than zero characters"})
+  }
+
+})
 
 module.exports = router;
